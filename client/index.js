533fbdb974f88ab789fce768f6b4bf3c502bc114
Mr-Dutchman <meelisfidelis@gmail.com>
2021-02-04 12:17:56 +0100

    adding delete and update functionality

--- client/index.js
+++ client/index.js
@@ -1,13 +1,99 @@
-document.addEventListener('DOMContentLoaded', function(){
-    fetch('http://localhost:5000/getALL')
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('http://localhost:5000/getAll')
     .then(response => response.json())
-    .then(data =>console.log(data));
-    loadHTMLTable([]);
+    .then(data => loadHTMLTable(data['data']));
+
 });
-function loadHTMLTable(data){
-    const table =document.querySelector('table tbody');
-    let tableHTMl = "";
-    if (data.lenght ===0){
-        table.innerHTML = "<tr><tr class = 'no_data' colspan ='5'>No Data</tr></tr>"
+
+
+const addBtn = document.querySelector('#add-name-btn');
+document.querySelector('table tbody').addEventListener('click', function(event){
+    if (event.target.className === 'delete-row-btn') {
+        deleteRowById(event.target.dataset.id);
     }
+
+});
+function deleteRowById(id){
+    fetch('http://localhost:5000/delete/'+ id, {
+        method: 'DELETE'
+    })
+    .then(response =>response.json())
+    .then(data => {
+        if (data.success) {
+            location.reload();
+        }
+    });
+}
+
+addBtn.onclick =function () {
+    const nameInput = document.querySelector('#name-input');
+    const name = nameInput.value;
+    nameInput.value ="";
+
+    fetch('http://localhost:5000/insert', {
+        headers:{
+            'content-type': 'application/json'
+        },
+        method:'POST', 
+        body:JSON.stringify({name : name})
+    })
+    .then(response => response.json())
+    .then(data => insertRowIntoTable(data['data']));
+}
+
+function insertRowIntoTable(data){
+    const table = document.querySelector("tbody");
+    const isTableData = table.querySelector('.no-data');
+
+    let tableHtml ="<tr>";
+
+    for (var key in data){
+        if (data.hasOwnProperty(key)){
+            if (key === 'dataAdded'){
+                date[key] =new Date(data[key]).toLocaleString();
+            }
+            tableHtml += ` <td>${data[key]}</td>`;
+        }
+    }
+   
+        tableHtml += `<td><button class ="delete-row-btn" data-id = ${data.id}>Delete</td>`;
+        tableHtml += `<td><button class ="edit-row-btn" data-id = ${data.id}>Edit></td>`;
+        
+ 
+
+    tableHtml += "</tr>";
+
+    if (isTableData){
+        table.innerHTML=tableHtml;
+    }else{
+        const newRow = table.insertRow();
+        newRow.innerHTML = tableHtml;
+    }
+
+
+
+}
+
+function loadHTMLTable(data) {
+    const table = document.querySelector('tbody');
+   
+    
+    if (data.lenght === 0) {
+        table.innerHTML = '<tr><td class = "no-data" colspan ="5">No Data</td></tr>';
+        return;
+    }
+
+    let tableHtml = "";
+
+    data.forEach(function ({id, name, date_added}){
+        tableHtml += "<tr>";
+        tableHtml += `<td>${id}</td>`;
+        tableHtml += `<td>${name}</td>`;
+        tableHtml += `<td>${new Date(date_added).toLocaleString()}</td>`;
+        tableHtml += `<td><button class ="delete-row-btn" data-id = ${id}>Delete</td>`;
+        tableHtml += `<td><button class ="edit-row-btn" data-id = ${id}>Edit></td>`;
+        
+      table.innerHTML +=  "</tr>";
+    });
+    table.innerHTML = tableHtml;
 }
--- server/app.js
+++ server/app.js
@@ -1,17 +1,48 @@
 const express = require('express');
-const app =express();
+const app = express();
 const cors = require('cors');
-const dotenv =require('dotenv');
+const dotenv = require('dotenv');
 dotenv.config();
 
+const dbService = require('./dbService');
+
 app.use(cors());
-app.use(express.json())
-app.use(express.urlencoded({extended: false}));
+app.use(express.json()); 
+app.use(express.urlencoded({extended : false}));
 
 //create
+app.post('/insert', (request, response) => {
+    const {name} = request.body;
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.insertNewName(name);
+    result
+    .then(data => response.json({data: data}))
+    .catch(err => console.log(err));
+});
 
 //read
-app.get('/getALL', (request, response) => {
-    response.json({ success :true});
+app.get('/getAll', (request, response) => {
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.getAllData();
+    
+    result 
+    .then(data => response.json({data : data}))
+    .catch(err => console.log(err));
+
+    
 })
+//delete
+app.delete('/delete/:id', (request, response) =>{
+    const{id}= request.params;
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.deleteRowById(id);
+    
+    result 
+    .then(data => response.json({success : data}))
+    .catch(err => console.log(err));
+})
+
 app.listen(process.env.PORT, () => console.log('app is running'));
